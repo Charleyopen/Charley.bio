af3e88c0ff422ccd00382d4f79787c78b0b7d7e3
charley <augustc@yeah.net>
2024-09-27 21:18:39 +0800

    Initial commit

---
+++ interact.js
@@ -0,0 +1,7 @@
+   // 示例：添加一个简单的交互
+   document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header h1');
+    header.addEventListener('click', function() {
+        alert('谢谢您访问我的网页！');
+    });
+});
